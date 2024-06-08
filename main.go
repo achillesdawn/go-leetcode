8d029cb09b277e721d9e05a79eb4b765383882c6
miguel <vackap@gmail.com>
2024-06-08 14:11:51 -0300

    perf first test

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 func isNStraightHand(hand []int, groupSize int) bool {
 
+	fmt.Println("hand", hand, "size", groupSize)
+
 	totalGroups := len(hand) / groupSize
 
 	if totalGroups < 1 {
@@ -15,35 +17,60 @@ func isNStraightHand(hand []int, groupSize int) bool {
 		return false
 	}
 
-	slices.Sort[[]int](hand)
+	var numMap map[int]int = make(map[int]int)
 
-	end := len(hand)
-
-	for range totalGroups {
-
-		current := hand[0]
-		hand = hand[1:]
-		end -= 1
-
-		for range groupSize - 1 {
-
-			idx, found := slices.BinarySearch(hand[:end], current+1)
-
-			if !found {
-				return false
-			}
-
-			slices.Delete(hand, idx, idx+1)
-			end -= 1
-			current += 1
+	for _, item := range hand {
+		value, exists := numMap[item]
+		if exists {
+			numMap[item] = value + 1
+		} else {
+			numMap[item] = 1
 		}
+	}
+
+	keys := make([]int, 0, len(numMap))
+	for k := range numMap {
+		keys = append(keys, k)
+	}
+
+	slices.Sort(keys)
+
+	for _, key := range keys {
+
+		count := numMap[key]
+		if count == 0 {
+			continue
+		}
+		fmt.Println("checking key", key, "for", count, "times", numMap)
+
+		for range count {
+			check := key
+			for range groupSize {
+				fmt.Println("checking key", check)
+				value, exists := numMap[check]
+
+				if !exists {
+					fmt.Println(check, "does not exist")
+					return false
+				}
+
+				if value < 0 {
+					fmt.Println("less than zero")
+					return false
+				}
+
+				numMap[check] = value - 1
+				check += 1
+			}
+		}
+
 	}
 
 	return true
 }
 
 func main() {
-	nums := []int{1, 2, 3, 4, 7, 8}
+	nums := []int{1, 1, 2, 2, 3, 3, 3, 4}
 	result := isNStraightHand(nums, 2)
 	fmt.Println(result)
 }
